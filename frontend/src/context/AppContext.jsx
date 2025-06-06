bf699c5371153fdbcef8b7f1369ffc7c49d0be16
יהודית טמינו <taminojudit@hotmail.co.il>
2025-06-06 12:54:45 +0300

    bottom banner

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useEffect, useState } from 'react';
 import { dummyProducts } from '../assets/assets';
 import toast from 'react-hot-toast';
